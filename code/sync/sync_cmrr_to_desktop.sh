184ba07451118761ddaa2e82f90205ae7b3d4492
Guest <dguest2@urmc-sh.rochester.edu>
2025-08-11 18:10:54 -0400

    Draft of NIFTI sync code

--- code/sync/sync_cmrr_to_desktop.sh
+++ code/sync/sync_cmrr_to_desktop.sh
@@ -6,3 +6,6 @@ scp dguest@surly.cmrr.umn.edu:~/Guestetal2021/figs_output_bulk/fsaverage*.png C:
 
 # Move bulk images from supplemental stability assessment of sub_to_cor results
 scp dguest@surly.cmrr.umn.edu:~/Guestetal2025_NSDPulvinar/figures_bulk/*supplemental*.png C:\Users\Daniel\Desktop\Guestetal2025_NSDPulvinar\figures_bulk
+
+# Move NIFTIs containing correlation results to disk
+scp dguest@surly.cmrr.umn.edu:/home/surly-raid3/dguest-data/Guestetal2021_data/subj01/mni/corr_cor_to_sub_hemi_*.nii.gz C:\Users\Daniel\Desktop\Guestetal2025_NSDPulvinar\?
