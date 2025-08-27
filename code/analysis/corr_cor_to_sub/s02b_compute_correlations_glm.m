718e528618eabc3e7a2dd7f6b5490cb6c5d03342
Daniel R. Guest <guest121@umn.edu>
2025-08-27 19:30:54 -0400

    Removed redundant loop from GLM analysis

--- code/analysis/corr_cor_to_sub/s02b_compute_correlations_glm.m
+++ code/analysis/corr_cor_to_sub/s02b_compute_correlations_glm.m
@@ -1,4 +1,4 @@
-% analyses/corr_cor_to_sub/s03b_compute_correlations_partial.m
+% analyses/corr_cor_to_sub/s03b_compute_correlations_glm.m
 %
 % Computes correlations between responses in a block containing the visual thalamus and
 % surrounding tissue and responses in average cortical seeds. This version of the script is
@@ -32,68 +32,65 @@ for subj=1:8
         % First, subset the cortical data to only be this subject and hemisphere, giving (30000, 14) matrix
         data_cortical = squeeze(avg_data(:, hemi, subj, :));
 
-        % Next, loop over label
-		for label=1:14
-            % Display current state
-			fprintf(['Subj: ' num2str(subj) ', Hemi: ' num2str(hemi) ', Label: ' num2str(label) '\n']);
+        % Display current state
+        fprintf(['Subj: ' num2str(subj) ', Hemi: ' num2str(hemi) '\n']);
 
-			% ===== METHOD #1 =====
-			% Calculate partial correlations coefficients
-                % subcortical input is (n_trial, n_voxel)
-                % cortical target input is (n_trial, 1)
-                % cortical regressors input is (n_trial, 13)
-			corr = f_calc_glm(x(:, 1:(750*n_sess_per_subj(subj)))', data_cortical(1:(750*n_sess_per_subj(subj)), :)); 
+        % ===== METHOD #1 =====
+        % Calculate partial correlations coefficients
+            % subcortical input is (n_trial, n_voxel)
+            % cortical target input is (n_trial, 1)
+            % cortical regressors input is (n_trial, 13)
+        corr = f_calc_glm(x(:, 1:(750*n_sess_per_subj(subj)))', data_cortical(1:(750*n_sess_per_subj(subj)), :)); 
 
-            % Output `corr` is (56*22*27, 15) matrix; we will loop through the columns and
-            % save them as a NIFTI file
-            for idx_beta = 1:15
-                % Create empty volume and subvolume to store results
-                subvol = zeros(56, 22, 27);
-                vol = zeros(182, 218, 182);	
+        % Output `corr` is (56*22*27, 15) matrix; we will loop through the columns and
+        % save them as a NIFTI file
+        for idx_beta = 1:15
+            % Create empty volume and subvolume to store results
+            subvol = zeros(56, 22, 27);
+            vol = zeros(182, 218, 182);	
 
-                % Embed data brick in MNI space
-                subvol(:) = corr(:, idx_beta);
-                vol(coords_start(1):coords_end(1), ...
-                    coords_start(2):coords_end(2), ...
-                    coords_start(3):coords_end(3)) = subvol;
+            % Embed data brick in MNI space
+            subvol(:) = corr(:, idx_beta);
+            vol(coords_start(1):coords_end(1), ...
+                coords_start(2):coords_end(2), ...
+                coords_start(3):coords_end(3)) = subvol;
 
-                % Save to disk as nifti 
-                nsd_savenifti(vol, [1, 1, 1], [data_dir 'subj0' num2str(subj) '/mni/corr_cor_to_sub_hemi_' num2str(hemi) '_label_' num2str(label) '_method_1_glm_beta=' num2str(idx_beta) '.nii.gz'], 1, [92, 127, 73]);
-            end
+            % Save to disk as nifti 
+            nsd_savenifti(vol, [1, 1, 1], [data_dir 'subj0' num2str(subj) '/mni/corr_cor_to_sub_hemi_' num2str(hemi) '_method_1_glm_beta=' num2str(idx_beta) '.nii.gz'], 1, [92, 127, 73]);
+        end
 
-			% ===== METHOD #2 =====
-			[trial_idxs_subcortical, trial_idxs_cortical] = f_select_trials_method2(subj, n_sess_per_subj(subj));
+        % ===== METHOD #2 =====
+        [trial_idxs_subcortical, trial_idxs_cortical] = f_select_trials_method2(subj, n_sess_per_subj(subj));
 
-			% Create empty matrix for correlations
-			corrs = zeros(size(x, 1), 15, 6); 
+        % Create empty matrix for correlations
+        corrs = zeros(size(x, 1), 15, 6); 
 
-			% Calculate correlations
-			for repeat=1:6
-				idxs_sub = trial_idxs_subcortical(:, repeat);
-				idxs_cor = trial_idxs_cortical(:, repeat);
-				idxs_sub = idxs_sub(~isnan(idxs_sub));
-				idxs_cor = idxs_cor(~isnan(idxs_cor));
-				corrs(:, :, repeat) = ...
-                    f_calc_glm(x(:, idxs_sub)', data_cortical(idxs_cor, :));  
-			end
-			corr = nanmean(corrs, 3);
+        % Calculate correlations
+        for repeat=1:6
+            idxs_sub = trial_idxs_subcortical(:, repeat);
+            idxs_cor = trial_idxs_cortical(:, repeat);
+            idxs_sub = idxs_sub(~isnan(idxs_sub));
+            idxs_cor = idxs_cor(~isnan(idxs_cor));
+            corrs(:, :, repeat) = ...
+                f_calc_glm(x(:, idxs_sub)', data_cortical(idxs_cor, :));  
+        end
+        corr = nanmean(corrs, 3);
 
-            % Output `corr` is (56*22*27, 15) matrix; we will loop through the columns and
-            % save them as a NIFTI file
-            for idx_beta = 1:15
-                % Create empty volume and subvolume to store results
-                subvol = zeros(56, 22, 27);
-                vol = zeros(182, 218, 182);	
+        % Output `corr` is (56*22*27, 15) matrix; we will loop through the columns and
+        % save them as a NIFTI file
+        for idx_beta = 1:15
+            % Create empty volume and subvolume to store results
+            subvol = zeros(56, 22, 27);
+            vol = zeros(182, 218, 182);	
 
-                % Embed data brick in MNI space
-                subvol(:) = corr(:, idx_beta);
-                vol(coords_start(1):coords_end(1), ...
-                    coords_start(2):coords_end(2), ...
-                    coords_start(3):coords_end(3)) = subvol;
+            % Embed data brick in MNI space
+            subvol(:) = corr(:, idx_beta);
+            vol(coords_start(1):coords_end(1), ...
+                coords_start(2):coords_end(2), ...
+                coords_start(3):coords_end(3)) = subvol;
 
-                % Save to disk as nifti 
-                nsd_savenifti(vol, [1, 1, 1], [data_dir 'subj0' num2str(subj) '/mni/corr_cor_to_sub_hemi_' num2str(hemi) '_label_' num2str(label) '_method_2_glm_beta=' num2str(idx_beta) '.nii.gz'], 1, [92, 127, 73]);
-            end
+            % Save to disk as nifti 
+            nsd_savenifti(vol, [1, 1, 1], [data_dir 'subj0' num2str(subj) '/mni/corr_cor_to_sub_hemi_' num2str(hemi) '_method_2_glm_beta=' num2str(idx_beta) '.nii.gz'], 1, [92, 127, 73]);
 		end
 	end
 end
